22fd1e0037617699ace2581152a1ea1fb2f55a85
Ben Dechrai <ben@dechrai.com>
2020-04-21 10:34:25 +1000

    Reload comments after add/delete and fix up comment layout.

--- src/components/comments.js
+++ src/components/comments.js
@@ -5,32 +5,35 @@ const Comments = ({slug}) => {
 
     slug = slug.replace(/^\/|\/$/g, '')
 
+    const loadComments = async (slug) => {
+        const comments = await fetch(process.env.GATSBY_API + "/comments/" + slug)
+            .then(res => res.json())
+        setComments(comments)
+    }
+
     useEffect(() => {
-        async function fetchComments(slug) {
-            const comments = await fetch(process.env.GATSBY_API + "/comments/" + slug)
-                .then(res => res.json())
-            setComments(comments)
-        }
-        fetchComments(slug)
+        loadComments(slug)
     }, [slug])
 
     return (
         <>
             <div className="comments">
                 {comments.map(comment => (
-                    <div className="comment" key={comment.id}>
-                        <p style={{ textAlign: `right`, marginBottom: `0px`, fontSize: `8px` }}>
+                    <div className="comment" key={comment.id} style={{ marginBottom: `1.75rem` }}>
+                        <blockquote style={{ marginBottom: `0` }}>
+                            {comment.comment}
+                            <p style={{ textAlign: `right` }}>&mdash; {comment.author}</p>
+                        </blockquote>
+                        <p style={{ textAlign: `right`, marginBottom: `0px`, marginRight: `1.75rem`, fontSize: `8px` }}>
                             <a href="#" onClick={(event) => {
                                 event.preventDefault()
                                 fetch(process.env.GATSBY_API + "/comments/" + slug + "/" + comment.id, {
                                     method: 'DELETE',
+                                }).then(res => {
+                                    loadComments(slug)
                                 })
                             }}>delete</a>
                         </p>
-                        <blockquote>
-                            {comment.comment}
-                            <p style={{ textAlign: `right` }}>&mdash; {comment.author}</p>
-                        </blockquote>
                     </div>
                 ))}
             </div>
@@ -46,6 +49,8 @@ const Comments = ({slug}) => {
                         author: event.target.author.value,
                         comment: event.target.comment.value,
                     }),
+                }).then(res => {
+                    loadComments(slug)
                 })
             }}>
                 <div>
